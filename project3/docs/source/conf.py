0e288eed6f2e77bb581751c32ee9c7f0c2a0c115
Luis Barroso <82448295+luismbarroso@users.noreply.github.com>
2021-04-22 14:38:16 +0100

    Update conf.py

--- project3/docs/source/conf.py
+++ project3/docs/source/conf.py
@@ -6,6 +6,8 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import_module(sphinx_material)
+
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
