89fbedd92585c9e4cd447633dba101302cbe778c
Luis Barroso <82448295+LMigu3liPT@users.noreply.github.com>
2021-05-10 20:56:51 +0100

    Upgrade 10/5/2021, 20h56

--- project/docs/source/conf.py
+++ project/docs/source/conf.py
@@ -91,10 +91,29 @@ html_theme_options = {
     "color_accent": "orange", #"cyan",
     "theme_color": "4b231b", #"#2196f3",
     "master_doc": False,
-    #"heroes": {
-    #    "index": "Instituto Politécnico de Setúbal",
-    #}
-    }
+    "nav_links": [
+        {"href": "index", "internal": True, "title": "Material"},
+        {
+            "href": "https://squidfunk.github.io/mkdocs-material/",
+            "internal": False,
+            "title": "Material for MkDocs",
+        },
+    ],
+    "heroes": {
+        "index": "A responsive Material Design theme for Sphinx sites.",
+        "customization": "Configuration options to personalize your site.",
+    },
+    "version_dropdown": True,
+    "version_json": "_static/versions.json",
+    "version_info": {
+        "Release": "https://bashtage.github.io/sphinx-material/",
+        "Development": "https://bashtage.github.io/sphinx-material/devel/",
+        "Release (rel)": "/sphinx-material/",
+        "Development (rel)": "/sphinx-material/devel/",
+    },
+    "table_classes": ["plain"],
+}
+
 
 language = "en"
 html_last_updated_fmt = ""
